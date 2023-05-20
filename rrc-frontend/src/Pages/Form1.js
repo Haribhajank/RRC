82ff82b40e9f3f99377efd914793660a333bb5cb
Raunit Singh <raunitsingh@Raunits-MacBook-Air.local>
2023-05-20 19:03:10 +0530

    minor change

--- rrc-frontend/src/Pages/Form1.js
+++ rrc-frontend/src/Pages/Form1.js
@@ -24,7 +24,7 @@ function Form1() {
 
             setInputFields([...inputFields, newfield]);
             setMemberLimit(prev => {
-                prev=prev-1;
+                prev = prev - 1;
                 return prev;
             })
         }
@@ -32,8 +32,11 @@ function Form1() {
     return (
         <>
             <Nav />
+            <div className='flex mx-16 md:my-40 lg:my-40 px-auto py-36 justify-center align-center lg:text-6xl md:text-6xl text-4xl'>
+                Registrations Starting Soon....
+            </div>
             {/* Header */}
-            <div className='head lg:grid lg:grid-cols-2 flex justify-center lg:overflow-hidden flex-wrap '>
+            {/* <div className='head lg:grid lg:grid-cols-2 flex justify-center lg:overflow-hidden flex-wrap '>
                 <div className='my-10 ml-20'>
                     <div className='flex flex-wrap justify-center align-center text-4xl mt-20 pr-24 font-black'>TAKE PART IN THE CHALLENGE AND COMPETE WITH STUDENTS FROM OTHER SCHOOLS</div>
                     <div className='mt-5'>The main problem that needs to be addressed in designing a best-out-of-waste project is to create a product that is functional, aesthetically pleasing, and eco-friendly. The design should be innovative and cost-effective and use readily available waste materials.</div>
@@ -43,11 +46,11 @@ function Form1() {
                         <img className='max-w-s ' src={require('../images/Rectangle 7.png')}></img>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
 
             {/* Form */}
-            <div className='my-10 py-10 '>
+            {/* <div className='my-10 py-10 '>
                 <div className='lg:mx-72 mx-24 justify-center align-center'>
                     <form>
                         {inputFields.map((input, index) => {
@@ -100,7 +103,7 @@ function Form1() {
                             )
                         })}
 
-                        {!memberLimit ? null:<div className='mt-8'>
+                        {!memberLimit ? null : <div className='mt-8'>
                             <button onClick={addFields} type="button" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"> + Add Member </button>
                         </div>}
                     </form>
@@ -108,7 +111,7 @@ function Form1() {
                         <Link to="/form2"><button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-12 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Next </button></Link>
                     </div>
                 </div>
-            </div>
+            </div> */}
             <Footer2 />
         </>
     )

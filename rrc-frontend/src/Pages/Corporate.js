f3a8b3bdf7477848d203b34816883bccb9322e82
Haribhajan <haribhajank5@gmail.com>
2023-05-22 11:04:26 +0530

    lkc

--- rrc-frontend/src/Pages/Corporate.js
+++ rrc-frontend/src/Pages/Corporate.js
@@ -21,16 +21,36 @@ function Corporate () {
             </div>
             <div className=' lg:mx-40 mx-auto px-10 justify-center align-center my-10  py-10 z-10'>
             <div className='text-4xl mb-5'>Corporate Team</div>
-                <div className='flex flex-wrap justify-center align-center lg:gap-52 gap-10 md:gap-32 mb-10 mt-10'>
+                <div className='flex flex-wrap justify-center align-center gap-16 mb-10 mt-10'>
                     <div>
-                        <div className='text-2xl'>Ayush Mothiya</div>
-                        <div>Phone Number: 9560571410</div>
-                        <div>Email : a.mothiya@iitg.ac.in</div>
+                        <div className='text-2xl'>Dhruva Kuthari</div>
+                        <div>Phone Number: 8999644302</div>
+                        <div>Email : k.dhruva@iitg.ac.in</div>
                     </div>
                     <div>
-                        <div className='text-2xl'>Rohak Jain</div>
-                        <div>Phone Number: 8285940695</div>
-                        <div>Email : j.rohak@iitg.ac.in</div>
+                        <div className='text-2xl'>Bhawana</div>
+                        <div>Phone Number: 8851258602</div>
+                        <div>Email : bhawana.mech22@iitg.ac.in</div>
+                    </div>
+                    <div>
+                        <div className='text-2xl'>Chandan</div>
+                        <div>Phone Number: 9142413091</div>
+                        <div>Email : chandan.kmr@iitg.ac.in</div>
+                    </div>
+                    <div>
+                        <div className='text-2xl'>Ambuj Mishra</div>
+                        <div>Phone Number: 9773971295</div>
+                        <div>Email : m.ambuj@iitg.ac.in</div>
+                    </div>
+                    <div>
+                        <div className='text-2xl'>Sarvagna Grandhi</div>
+                        <div>Phone Number: 9492874964 </div>
+                        <div>Email : g.sarvagna@iitg.ac.in</div>
+                    </div>
+                    <div>
+                        <div className='text-2xl'>Amit patel</div>
+                        <div>Phone Number: 9179148110 </div>
+                        <div>Email : p.amit@iitg.ac.in</div>
                     </div>
             </div>
             </div>
